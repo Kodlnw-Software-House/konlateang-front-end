4795215370473789026bf201dfc6a11e3384a556
Nachanon Montikanon <nachanon.montikanon@mail.kmutt.ac.th>
2021-09-05 01:11:34 +0700

    [UPDATE] MainPage Search and Filter

--- my-app/src/pages/withAuth/MainPage/MainPage.js
+++ my-app/src/pages/withAuth/MainPage/MainPage.js
@@ -1,24 +1,35 @@
 import ItemCard from "../../../components/ui/ItemCard";
 import useFetch from "../../../hooks/use-fetch";
-import { RefreshIcon } from "@heroicons/react/outline";
 import CovidInfo from "../../../components/MainPage/Covid19Info";
 import ActiveHospital from "../../../components/MainPage/ActiveHospital";
 import Card from "../../../components/ui/Card";
+import {
+  SearchCircleIcon,
+  RefreshIcon,
+  AdjustmentsIcon,
+} from "@heroicons/react/outline";
 const MainPage = () => {
-  const { data: covidData, loading } = useFetch(
-    "https://covid19.ddc.moph.go.th/api/Cases/today-cases-all"
-  );
+  const {
+    data: covidData,
+    error,
+    loading,
+  } = useFetch("https://covid19.ddc.moph.go.th/api/Cases/today-cases-all");
 
   return (
     // Covid19 Todays
     <div>
       <ItemCard>
-        {loading && !covidData && (
+        {loading && (
           <div className="mx-auto">
             <RefreshIcon className="w-10 h-10 animate-spin" />
           </div>
         )}
-        {!loading && covidData && (
+        {error && (
+          <div className="mx-auto">
+            <span>Error : {error}</span>
+          </div>
+        )}
+        {!loading && covidData && !error && (
           <CovidInfo
             newCase={covidData[0].new_case}
             updateDate={covidData[0].update_date}
@@ -29,11 +40,30 @@ const MainPage = () => {
           />
         )}
       </ItemCard>
+      {/* search input */}
+      <Card>
+        <div class="form-control shadow-sm">
+          <div class="relative">
+            <input
+              type="text"
+              placeholder="ค้นหาศูนย์พักคอย"
+              class="w-full pr-16 input input-sm rounded-box"
+            />
+            <button class="absolute top-0 right-0 rounded-l-none btn btn-sm btn-ghost">
+              <SearchCircleIcon className="w-7 h-auto" />
+            </button>
+          </div>
+        </div>
+        <div className="border-b-2 border-primary flex items-center py-2">
+          <AdjustmentsIcon className="w-7 h-auto" />
+          <span className="text-xl">ตัวกรอง</span>
+        </div>
+      </Card>
+      {/* active hospital list */}
       <div>
         <Card>
           <p className="text-xl">ศูนย์พักคอยที่เปิดรับ</p>
         </Card>
-        {/* active hospital list */}
         <ActiveHospital
           hospitalPic="http://daisyui.com/tailwind-css-component-profile-1@94w.png"
           hospitalName="โรงพยาบาลนครธน"
